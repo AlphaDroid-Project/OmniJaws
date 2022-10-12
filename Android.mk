7aad299cf85bfa07cbf4ce3095e588d51744e5fc
minaripenguin37 <alexfinhart@gmail.com>
2022-10-12 09:41:30 +0800

    Omnijaws: Fix default-permissions module path
    10-12 08:01:42.846  4233  4266 W SystemConfig: Got exception parsing permissions.
    10-12 08:01:42.846  4233  4266 W SystemConfig: org.xmlpull.v1.XmlPullParserException: Unexpected start tag in /system_ext/etc/permissions/default_permissions_org.omnirom.omnijaws.xml: found exceptions, expected 'permissions' or 'config'
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- Android.mk
+++ Android.mk
@@ -41,7 +41,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := privapp_whitelist_org.omnirom.omnijaws-ext.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_SYSTEM_EXT_ETC)/permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_SYSTEM_EXT_ETC)/default-permissions
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
